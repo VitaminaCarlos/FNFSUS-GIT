7edffa2f59d5a96c26e6770095a1652d59065ad9
VitaminaCarlos <77900691+VitaminaCarlos@users.noreply.github.com>
2021-12-01 21:45:09 -0300

    sus!

---
+++ README.md
@@ -0,0 +1,3 @@
+hi retro
+hello Carlos
+https://cdn.discordapp.com/attachments/832423825076912128/904853482878664734/video0_-_2021-07-11T034201.295.mp4
---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,36 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('startScreen'));
+		add(bg);
+		
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			FlxG.switchState(new MainMenuState());
+		}
+
+		if (controls.BACK){
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+			
+		super.update(elapsed);
+	}
+}
